4ca46eed6eb67f43e919672995f85cf4c7a12525
Peter Andreas Entschev <peter@entschev.com>
2024-11-19 08:58:04 -0800

    Create `RESULTS_DIR` in `ci/test_wheel_cudf_polars.sh`
    Ensure `RESULTS_DIR` is created in `ci/test_wheel_cudf_polars.sh` when
    undefined, ensuring it runs when reproducing CI locally without need for
    extra steps.

--- ci/test_wheel_cudf_polars.sh
+++ ci/test_wheel_cudf_polars.sh
@@ -25,6 +25,10 @@ python -m pip install \
     "$(echo ./dist/libcudf_${RAPIDS_PY_CUDA_SUFFIX}*.whl)" \
     "$(echo ./dist/pylibcudf_${RAPIDS_PY_CUDA_SUFFIX}*.whl)"
 
+RESULTS_DIR=${RAPIDS_TESTS_DIR:-"$(mktemp -d)"}
+RAPIDS_TESTS_DIR=${RAPIDS_TESTS_DIR:-"${RESULTS_DIR}/test-results"}/
+mkdir -p "${RAPIDS_TESTS_DIR}"
+
 rapids-logger "Run cudf_polars tests"
 
 function set_exitcode()
